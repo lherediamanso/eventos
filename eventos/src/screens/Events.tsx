868135ff689a80dcede4d464217b448bf45e8caf
Lazaro <lheredia@cash.com.uy>
2023-11-17 22:10:57 -0300

    Cambio en listado de eventos

--- eventos/src/screens/Events.tsx
+++ eventos/src/screens/Events.tsx
@@ -19,16 +19,7 @@ const Events = ({ route, navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.scrollView}>
-
         <View >
-
-          <View style={styles.containerBlue}>
-            <View style={styles.cajaPorcentaje}><Text>1</Text></View>
-            <View style={styles.cajaPorcentaje}><Image
-              style={styles.img}
-              source={require('../../assets/icon.png')}
-            /></View>
-          </View >
           <View style={styles.containerGreen}>
             <ScrollView
               horizontal={true}
@@ -53,24 +44,8 @@ const Events = ({ route, navigation }) => {
                 )}
               />
             </ScrollView>
-
           </View>
-
-
         </View>
-
-        <View>
-          <Text style={styles.text}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-        </View>
-
       </ScrollView>
     </SafeAreaView>
   )
@@ -83,91 +58,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 16,
     marginBottom: 4
   },
-
+  scrollView: {
+    flex: 1,
+    marginHorizontal: 20,
+  },
+  containerGreen: {
+    flex: 1,
+    flexDirection: 'column',
+  },
   box: {
     width: 100,
     height: 100,
     backgroundColor: 'gray',
     margin: 2
-  },
-  containerBlue: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    backgroundColor: 'blue'
-  },
-  containerGreen: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: 'green'
-  },
-  cajaPorcentaje: {
-    width: '50%',
-    height: '35%',
-    margin: 2,
-    backgroundColor: 'gray',
-  },
-  caja: {
-    flex: 1,
-
-    fontSize: 24,
-    fontWeight: 'bold',
-    paddingHorizontal: 8
-  },
-  img: {
-    width: '100%',
-    height: '100%',
-  },
-  card: {
-
-    paddingHorizontal: 8,
-    justifyContent: 'center',
-    backgroundColor: '#8D3DAF',
-    alignItems: 'center',
-    width: 300,
-    height: 300,
-    borderRadius: 10,
-    margin: 8,
-    marginTop: 10
-  },
-  headingText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    paddingHorizontal: 8
-  },
-  userCard: {
-    flex: 1,
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 3,
-    backgroundColor: '#8D3DAF',
-    padding: 8,
-    borderRadius: 10
-  },
-  userImage: {
-    width: 60,
-    height: 60,
-    borderRadius: 60 / 2,
-    marginRight: 14
-  },
-  userName: {
-    fontSize: 16,
-    fontWeight: '600',
-    color: '#FFF'
-  },
-  userStatus: {
-    fontSize: 12
-  },
-  scrollView: {
-    flex: 1,
-    backgroundColor: 'pink',
-    marginHorizontal: 20,
-  },
-  text: {
-    fontSize: 30,
-    marginTop: 14,
-    margin: 8
-
   },
 });
 
